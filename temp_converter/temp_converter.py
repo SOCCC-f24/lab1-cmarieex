bc276d99a8963f5fe76d3b09ef5a9cfeab8f95bb
cmarieex <80868778+cmarieex@users.noreply.github.com>
2024-09-08 12:46:12 -0400

    Update temp_converter.py

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-
+"""
+    temp.py: converts celsius to fahrenheit
+    created by: christine roe
+    course number: csc138-en
+    created on: 09082024
+    changes made: added paranthesis added comments
+"""
 # process
+# conversion formula
 def c2f(c):
-    return c * 9 / 5 + 32
+    return c * (9 / 5) + 32
 
 def main(cel):
     return c2f(cel)
 
+# result
 if __name__ == "__main__":
     cel = 100         # input
     print(main(cel))  # output
