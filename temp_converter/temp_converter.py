f62decd84c034f1cae0a07d9cdd6fabf811124f9
cmarieex <80868778+cmarieex@users.noreply.github.com>
2024-09-10 09:24:55 -0400

    Update temp_converter.py

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -9,7 +9,7 @@
 # process
 # conversion formula
 def c2f(c):
-    return (c * 9 / 5) + 32
+    return c * (9 / 5) + 32
 
 def main(cel):
     return c2f(cel)
@@ -17,4 +17,4 @@ def main(cel):
 if __name__ == "__main__":
     cel = 100         # input
     print(main(cel))  # output
-    round(main(cel))
+   
