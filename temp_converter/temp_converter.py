4cdc5d985ee9011f65ced50de32195994f4772ae
cmarieex <80868778+cmarieex@users.noreply.github.com>
2024-09-11 13:55:54 -0400

    Update temp_converter.py

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -11,16 +11,18 @@
 # def c2f(c):
 # return c * (9.0 / 5.0) + 32.0 #pemdas
 
-def main():
-	f = 0
-	c = f2c(f)
-print(f"{f}F is {c} C")
-    
 def f2c_raw(f): 
 	return f - 32 * 5 / 9
+	
 def f2c_op(f): 
 	return (f - 32) * 5 / 9 #pemdas 
     
+def main():
+	f = 0
+	c = f2c(f)
+print(f"{f}F is {c} C") 
+    
 if __name__ == "__main__":
+	main() 
    
    
